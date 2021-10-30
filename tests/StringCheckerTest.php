047b105fd68dee0626acbbb22d7fec31f26b4dc8
Dmitry Panfilov <d.panfilov@crpt.ru>
2021-10-30 18:36:46 +0300

    Editing for PSR-4

---
+++ tests/StringCheckerTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests;
+
+use Checker\StringChecker;
+use PHPUnit\Framework\TestCase;
+use http\Exception\InvalidArgumentException;
+
+
+/**
+ * Class StringCheckerTest
+ */
+class StringCheckerTest extends TestCase
+{
+    private const DEBUG_MODE = 0;
+    private const POSITIVE_STRING_CASES =
+        [
+            '()()()()((()()))',
+            '((((()()(()())))))',
+            '( ( ) ) \n ()()'
+        ];
+
+    private const NEGATIVE_STRING_CASES =
+        [
+            '()()()()((()())))',
+            '((((()()(()())))))1',
+            '( ( ) ) \n ()()*'
+        ];
+
+    /**
+     * @expectedException InvalidArgumentException
+     */    public function testStringChecker()
+    {
+        foreach (self::POSITIVE_STRING_CASES as $string) {
+            $checker = new StringChecker($string, self::DEBUG_MODE);
+            $this->assertEquals(true, $checker->stringCheck());
+        }
+        foreach (self::NEGATIVE_STRING_CASES as $string) {
+            $checker = new StringChecker($string, self::DEBUG_MODE);
+            $this->assertEquals(false, $checker->stringCheck());
+        }
+    }
+}
